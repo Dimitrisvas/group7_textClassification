93c7b6d503dee15ad75fe5219c93c7ed61d0e23e
dimitris <jim.vassiliou64@gmail.com>
2021-05-24 20:36:46 +0100

    Simplify testing process, add db and error testing

--- flask_implementation/test_flask_imp.py
+++ flask_implementation/test_flask_imp.py
@@ -1,38 +1,79 @@
-import os
-import tempfile
+import requests
 
-import pytest
+HOME_URL = 'http://localhost:5000/'
 
-from flask_implementation import create_app
+COMMENT = "it's me"
 
+def submit_comment(comment):
+    return requests.post(HOME_URL, data={
+        'comment_text': comment,
+        'formGroupExampleInput2':''
+    })
 
-@pytest.fixture
-def client():
-    app = create_app()
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
+def goto_predictions():
+    return requests.get(HOME_URL + 'predictions')
 
-    with app.test_client() as client:
-    #    with app.app_context():
-    #        init_db()
-        yield client
+def goto_comments():
+    return requests.get(HOME_URL + 'predictions')
 
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
+def predictions_test(COMMENT):
+    passed = False
+    r = submit_comment(COMMENT)
 
-def submit_comment(client, comment):
-    return client.post('/', data=dict(
-        comment_text=comment,
-        formGroupExampleInput2=""
-    ), follow_redirects=True)
+    r = goto_predictions()
+    if (COMMENT in r.text):
+        passed = True
 
-def goto_predictions(client):
-    return client.get('/predictions')
+    return passed
 
-def test_prediction(client):
-    comment = "you suck"
+def comments_test(COMMENT):
+    passed = False
+    COMMENTS = []
 
-    rv = submit_comment(client, comment)
+    for i in range(3):
+        COMMENTS.append(f'{[i]}-{COMMENT}')
+    r = submit_comment(COMMENTS[0])
 
-    rv = goto_predictions(client)
-    assert b'you suck' in rv.data
+    r = submit_comment(COMMENTS[1])
+
+    r = submit_comment(COMMENTS[2])
+
+    r = goto_comments()
+    if ((COMMENTS[0] in r.text) & (COMMENTS[1] in r.text) & (COMMENTS[2] in r.text)):
+        passed = True
+
+    return passed
+
+def error_test():
+    passed = False
+
+    r = submit_comment("")
+    if ("Comment is required." in r.text):
+        passed = True
+
+    return passed
+
+def run_tests(COMMENT):
+    count = 0
+    passed = []
+    passed.append(predictions_test(COMMENT))
+    if passed[0]:
+        print("Predictions Test Passed")
+        count += 1
+    else:
+        print("Predictions Test Failed")
+    passed.append(comments_test(COMMENT))
+    if passed[1]:
+        print("Comments Test Passed")
+        count += 1
+    else:
+        print("Comments Test Failed")
+    passed.append(error_test())
+    if passed[2]:
+        print("Error Test Passed")
+        count += 1
+    else:
+        print("Error Test Failed!")
+    print (f'Passed {count}/3 tests')
+
+run_tests(COMMENT)
